e45fb7b6ae6bfaf59f96c681142029c31166b482
praveen <karnapraveen22@gmail.com>
2024-05-22 22:04:39 +0545

    use of query basic

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+
+
 app =FastAPI()
 
-@app.get('/')
-def index():
-    return "hey"
+@app.get('/blog')
+def index(limit=10, published: bool= True):
+    if published:
+        return {'data': f'{limit} published blog list from db'}
+    else:
+        return {'data': f'{limit}  blog list from db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+
+@app.get('/blog/unpublished')
+def show(id: int ):
+    return {'data': 'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id: int ):
+    return {'data': id}
+
+@app.get ('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1','2'}}
